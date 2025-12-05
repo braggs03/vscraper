b71e64f6e088fa39cef0a018b986db82b22841d4
braggs03 <brandon@ragghianti.org>
2025-12-04 22:02:48 -0800

    Added type for download progress from backend.

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,11 @@
 interface Config {
     skip_homepage: boolean,
 }
+
+interface DownloadProgress {
+    url: string,
+    percent: string,
+    size_downloaded: string,
+    speed: string,
+    eta: string,
+}
